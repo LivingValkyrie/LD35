6b918a539bbd13ea9c17264852a188b7a930303b
LivingValkyrie <deadwynn@gmail.com>
2016-04-15 21:48:26 -0500

    added folder structure, basic prefabs and player state machine
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ LD35 - shapeshift/Assets/_Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.net
+/// 
+/// Description: Projectile 
+/// </summary>
+public class Projectile : MonoBehaviour {
+
+	#region Fields
+
+	public ProjectileType type;
+
+	#endregion
+	
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
+
+public enum ProjectileType {
+	Solid, Energy
+}
