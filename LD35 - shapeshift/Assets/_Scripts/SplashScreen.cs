aaf8f3b49e17b6f0c07c39b81e7a1bb5c4f9c9a2
LivingValkyrie <deadwynn@gmail.com>
2016-04-17 19:09:11 -0500

    added build stuff, splash screen and a few minor things
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ LD35 - shapeshift/Assets/_Scripts/SplashScreen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.net
+/// 
+/// Description: SplashScreen 
+/// </summary>
+public class SplashScreen : MonoBehaviour {
+
+	#region Fields
+
+	public float count = 3;
+
+	#endregion
+	
+	void Start () {
+	
+		Invoke("ChangeScene", count);
+
+	}
+	
+	void ChangeScene () {
+		SceneManager.LoadScene("Main Menu");
+	}
+}
